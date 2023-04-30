96abdada79dc7288cb0cad268a551cfc64225e9e
IshanPoudel <ishan.poudel@mavs.uta.edu>
2023-04-29 21:29:25 -0500

    start_router.py

--- port_script.py
+++ port_script.py
@@ -1,3 +1,7 @@
+#Assigns different port numbers to each node depending on the start port number
+
+port_number =5050
+
 #Read the config file and create a 2d array
 import sys
 import socket
@@ -18,13 +22,6 @@ class RouterIP:
     def add_neighbors(self , neighbor_dict):
         self.neighbor = neighbor_dict
 
-
-
-
-
-
-# ---------------------------------Read the config file --------------------------
-
 with open('network.config' , 'r') as f:
     matrix_config = f.read()
 
@@ -42,15 +39,11 @@ for line in lines:
         row.append(int(each_number))
     matrix.append(row)
 
-
-
-#Contains all RouterIP class objects (each class contains router IP and their neigbours)
-
 router_list=[]
 
 #Store the adjacency matrix as objects of RouterIp
 for index , matrix_row in enumerate(matrix):
-    router_config = "192.168.1." + str(index+1)
+    router_config = str(port_number+index) 
     router = RouterIP(router_config)
     
     neighbor_dict = {}
@@ -62,7 +55,7 @@ for index , matrix_row in enumerate(matrix):
         #Check if neighbor
         if weight!=0:
             #Add to a dictionary
-            neighbor_dict.update({"192.168.1." + str(i+1) : weight})  
+            neighbor_dict.update({  str(port_number+i) : weight})  
         i=i+1
     router.add_neighbors(neighbor_dict)
             
@@ -71,7 +64,7 @@ for index , matrix_row in enumerate(matrix):
     #Store routerIP object in a list 
     router_list.append(router)
 
-
+#Store the port number
 
 #Router list contains all the ip config , neighbors and cost
 for value in router_list:
@@ -81,15 +74,16 @@ for value in router_list:
 
 
 
-#-------------------OPEN A POSSIBLE UDP CONNECTION-----------
-# Define the port number to listen on
-PORT = 5050
+#Open a specific udp connection
+
+#Later on get this from the command line
+PORT =5050
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific port
-sock.bind(('192.168.1.100', PORT))
+sock.bind(('127.0.0.1', PORT))
 
 # Receive data from the socket
 data, addr = sock.recvfrom(1024)
